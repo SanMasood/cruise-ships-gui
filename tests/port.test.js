81e71c08e4aeac9c6049c6f21016ff735fbc41fd
SanMasood <sanamasood.giki@gmail.com>
2020-03-31 18:27:27 +0100

    Ship has port & name

--- tests/port.test.js
+++ tests/port.test.js
@@ -1,11 +1,24 @@
 const Port = require('../src/ship.js');
 
+let port;
+
+beforeEach(() => {
+
+port = new Port();
+});
+
 describe ('constructor', () => {
 
     it('creates instance of object Port', () => {
 
         expect(new Port()).toBeInstanceOf(Object);
 
+    })
+
+    it ('has a name', () => {
+        
+        port.name = 'Penzance';
+        expect(port.name).toBe('Penzance');
     })
 
 })
