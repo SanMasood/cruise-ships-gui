24cb0c2b7618783d8f2956bc24c8a26c5e8aeb50
SanMasood <sanamasood.giki@gmail.com>
2020-04-13 18:56:18 +0100

    initialization

---
+++ src/controller.js
@@ -0,0 +1,16 @@
+function Controller () {
+
+    this.initialiseSea();
+}
+
+
+Controller.prototype.initialiseSea = function initialiseSea (){
+
+        window.setInterval(() => {console.log('Hello');}, 500);
+        
+
+     
+
+
+    }
+
